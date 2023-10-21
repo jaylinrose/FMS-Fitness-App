f0da6eab6ae0f16023ac3d6b4916ac9c01915a3f
jaylinrose <jaylin.manuel02@gmail.com>
2023-10-21 13:02:32 -0700

    added and installed dotenv file, express app setup

---
+++ server/server.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+
+const express = require('express')
+
+//express app
+const app = express()
+
+//routes
+app.get('/', (req, res) => {
+    res.json({mssg: 'Welcome to the app'})
+})
+
+//listen for requests 
+app.listen(4800, () => {
+    console.log('listening on port 4000!!!!')
+})
+
+process.env
