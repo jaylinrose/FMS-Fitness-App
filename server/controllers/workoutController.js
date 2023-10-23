3671b50085fd9d5a121429afa6b3dce5b3b11e70
jaylinrose <jaylin.manuel02@gmail.com>
2023-10-23 14:30:56 -0700

    API endpoints and routes finished

--- server/controllers/workoutController.js
+++ server/controllers/workoutController.js
@@ -44,12 +44,41 @@ const createWorkout = async (req, res) => {
 // DELETE a workout
 
 const deleteWorkout = async (req, res) => {
+    const {id} = req.params
 
+    if (!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error: 'workout does not exist'})
+    }
+
+    const workout = await Workout.findOneAndDelete({_id: id})
+
+    if(!workout) {
+        return res.status(400).json({error: 'workout does not exist'})
+    }
+
+    res.status(200).json(workout)
 }
 
 // UPDATE a workout
 
 const updateWorkout = async (req, res) => {
+    const {id} = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error: 'workout does not exist'})
+    }
+
+    const workout = await Workout.findOneAndUpdate({_id: id}, {
+        ...req.body
+
+    })
+
+    if(!workout) {
+        return res.status(400).json({error: 'workout does not exist'})
+    }
+
+    res.status(200).json(workout)
+
 
 }
 
