dd0c040afd9d85aa7070ac8a6e54edb26c964134
jaylinrose <jaylin.manuel02@gmail.com>
2023-10-23 12:13:05 -0700

    controllers finished for GET all, GET single, and CREATE routes

--- server/controllers/workoutController.js
+++ server/controllers/workoutController.js
@@ -2,12 +2,46 @@ const Workout = require('../models/workoutModel')
 
 // GET all workouts
 
+const getWorkouts = async (req, res) => {
+    const workouts = await Workout.find({}).sort({createdAt: -1})
+
+    res.status(200).json(workouts)
+}
+
 // GET a single workout
 
-// CREATE new workout
+const getWorkout = async (req, res) => {
+    const {id} = req.params
 
+    const workout = await Workout.findById(id)
+
+    if(!workout) {
+        return res.status(404).json({error: 'workout does not exist'})
+    }
+
+    res.status(200).json(workout)
+}
+
+// CREATE new workout
+const createWorkout = async (req, res) => {
+    const {title, load, reps} = req.body
+    
+    // added doc to DB 
+    try{
+        const workout = await Workout.create({title, load, reps})
+        res.status(200).json(workout)
+    } catch(error) {
+        res.status(400).json({error: error.message})
+    }
+}
 
 
 // DELETE a workout
 
 // UPDATE a workout
+
+module.export = {
+    createWorkout,
+    getWorkout,
+    getWorkouts
+}
