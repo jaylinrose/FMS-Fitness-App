224b07eb5a83415b304bd352ac2e4ec19eeb008d
jaylinrose <jaylin.manuel02@gmail.com>
2023-10-23 18:48:37 -0700

    added workoutDetails to components, render successful

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,9 @@
 import { useEffect, useState } from "react"
 
+// components
+
+import WorkoutDetails from "../components/WorkoutDetails"
+
 const Home = () => {
     const [workouts, setWorkouts] = useState(null)
 
@@ -21,7 +25,7 @@ const Home = () => {
         <div className="home">
             <div className="workouts">
                 {workouts && workouts.map((workout) => (
-                    <p key={workout._id}> {workout.title} </p>
+                    <WorkoutDetails key={workout._id} workout={workout}/>
                 ))}
             </div>
         </div>
