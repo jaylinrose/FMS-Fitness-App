951798e3b8e4388b6408561686069e5de3bc1826
jaylinrose <jaylin.manuel02@gmail.com>
2023-10-26 17:34:38 -0700

    added userModel and userSchema

---
+++ server/models/workoutModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const workoutSchema = new Schema({
+    title: {
+        type: String,
+        requried: true
+    },
+    reps: {
+        type: Number,
+        requried: true
+    },
+    load: {
+        type: Number,
+        requried: true
+    }
+}, {timestamps: true} ) 
+
+module.exports = mongoose.model('Workout', workoutSchema)
