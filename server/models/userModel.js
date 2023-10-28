b1e1a1eed86e78734f2eb136318116f5237a4b7b
jaylinrose <jaylin.manuel02@gmail.com>
2023-10-28 10:10:12 -0700

    added hashing function and salt with bcrypt

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 
@@ -13,10 +15,31 @@ const userSchema = new Schema({
     password: {
         type: String,
         required: true,
-        unique: true
     }
-
 
 })
 
-module.exports = mongoose.Schema('User', userSchema)
+
+// static signup method
+
+if (!email || !password) {
+    throw Error('All fields must be filled')
+}
+
+userSchema.statics.signup = async function (email, password) {
+
+    const exists = await this.findOne({email})
+
+    if (exists) {
+        throw Error('Email already in use')
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+
+    const user = await this.create({email, password: hash})
+
+    return user
+}
+
+module.exports = mongoose.model('User', userSchema)
