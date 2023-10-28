788c02709aebb176a9eb2bb53dee7d2673e31714
jaylinrose <jaylin.manuel02@gmail.com>
2023-10-28 11:31:58 -0700

    added hooks and context for user authentication

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -5,39 +5,62 @@ const validator = require('validator')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
-
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-
-    password: {
-        type: String,
-        required: true,
-    }
-
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
 })
 
-
 // static signup method
+userSchema.statics.signup = async function(email, password) {
 
-if (!email || !password) {
+  // validation
+  if (!email || !password) {
     throw Error('All fields must be filled')
+  }
+  if (!validator.isEmail(email)) {
+    throw Error('Email not valid')
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error('Password not strong enough')
+  }
+
+  const exists = await this.findOne({ email })
+
+  if (exists) {
+    throw Error('Email already in use')
+  }
+
+  const salt = await bcrypt.genSalt(10)
+  const hash = await bcrypt.hash(password, salt)
+
+  const user = await this.create({ email, password: hash })
+
+  return user
 }
 
-userSchema.statics.signup = async function (email, password) {
+// static login method
 
-    const exists = await this.findOne({email})
-
-    if (exists) {
-        throw Error('Email already in use')
+userSchema.statics.login = async function(email, password) {
+    
+    if (!email || !password) {
+        throw Error('All fields must be filled')
     }
 
-    const salt = await bcrypt.genSalt(10)
-    const hash = await bcrypt.hash(password, salt)
+    const user = await this.findOne({email})
+    if (!user) {
+        throw Error('Incorrect email')
+    }
 
-    const user = await this.create({email, password: hash})
+    const match = await bcrypt.compare(password, user.password)
+    if (!match) {
+        throw Error('Incorrect password')
+    }
 
     return user
 }
