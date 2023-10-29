75b1363d6f3e6e31519eb67c48f62f12118e7938
jaylinrose <jaylin.manuel02@gmail.com>
2023-10-29 02:20:18 -0700

    finalization

--- server/server.js
+++ server/server.js
@@ -1,18 +1,39 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
+const userRoutes = require('./routes/user')
+
 
 //express app
 const app = express()
 
+//middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+
 //routes
-app.get('/', (req, res) => {
-    res.json({mssg: 'Welcome to the app'})
-})
+app.use('/api/workouts', workoutRoutes)
+app.use('/api/user', userRoutes)
 
-//listen for requests 
-app.listen(4800, () => {
-    console.log('listening on port 4000!!!!')
-})
 
-process.env
+
+// connect to DB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('connected to database')
+    // listen to port
+    app.listen(process.env.PORT, () => {
+      console.log('listening for requests on port', process.env.PORT)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  }) 
+
